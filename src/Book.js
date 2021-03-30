7768255cd4dc72512d29a606b41149ce5ee2156a
Alain Souchet <souchet.alain@gmail.com>
2021-03-30 19:00:54 +0200

    style: Add images cover

--- src/Book.js
+++ src/Book.js
@@ -40,9 +40,7 @@ class Book extends Component {
                         <div className="book-cover" style={{
                             width: 128,
                             height: 193,
-                            background: "gray"
-                            // TODO Display books' covers instead of a gray rectangles
-                            // backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
+                            backgroundImage: `url(${book.imageLinks.thumbnail})`
                         }}>
                         </div>
 
